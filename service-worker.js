544ed733a8aa984255921309a0af84afc7247044
scepstein <102193968+scepstein@users.noreply.github.com>
2026-03-16 19:33:32 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "liftlog-v05";
+const CACHE_NAME = "liftlog-v06";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -9,27 +10,35 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  // Activate the new service worker immediately
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    })
   );
+
+  // Take control of existing pages
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
